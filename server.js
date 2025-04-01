9ac4db4b4ed181631f3ff3920e957dc1dc0e718a
Maxx2774 <maxviggonordin@hotmail.se>
2025-04-01 23:45:59 +0200

    .

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(
       callback(null, true); // Allow all origins
     },
     credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE"],
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
