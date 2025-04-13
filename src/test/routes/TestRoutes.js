c419ff4b8f1aa7dcf4fb8841338f59f1a6712cc3
hassohamid <hassohamid@hotmail.com>
2025-04-13 18:28:21 +0200

    Recommit

--- src/test/routes/TestRoutes.js
+++ src/test/routes/TestRoutes.js
@@ -1,17 +1,9 @@
 const express = require("express");
-const {
-  addTestData,
-  deleteData,
-  addBulkTestData,
-  removeBulkTestData,
-} = require("../controllers/TestControllers");
+const { addTestData, deleteData } = require("../controllers/TestControllers");
 
 const router = express.Router();
 
 router.post("/admin/add-test-data", addTestData);
 router.delete("/admin/delete-test-data", deleteData);
-
-router.post("/admin/add-bulk-test-data", addBulkTestData);
-router.delete("/admin/remove-bulk-test-data", removeBulkTestData);
 
 module.exports = router;
